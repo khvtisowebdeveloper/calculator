ba6026ea0a750d9822ecb2013b755c493d03e6ea
khvtisowebdeveloper <119016695+khvtisowebdeveloper@users.noreply.github.com>
2023-02-12 22:19:28 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const calculator = document.querySelector(".calculator");
+const display = calculator.querySelector(".display");
+const buttons = calculator.querySelectorAll("button");
+
+buttons.forEach(button => {
+  button.addEventListener("click", function() {
+    let value = this.value;
+    let currentDisplay = display.textContent;
+    let newDisplay;
+
+    if (value === "clear") {
+      newDisplay = "0";
+    } else if (value === "backspace") {
+      newDisplay = currentDisplay.substring(0, currentDisplay.length - 1);
+    } else if (value === "=") {
+      newDisplay = eval(currentDisplay

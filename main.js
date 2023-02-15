373e6305800525e09c0241389688487c353ce89c
LevanNamicheishvili <levannnamicheishvili@gmail.com>
2023-02-15 11:15:56 +0400

    responsive fix
    esec sheni responsive xvtiso

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-const calculator = document.querySelector(".calculator");
-const display = calculator.querySelector(".display");
-const buttons = calculator.querySelectorAll("button");
+// const calculator = document.querySelector(".calculator");
+// const display = calculator.querySelector(".display");
+// const buttons = calculator.querySelectorAll("button");
 
-buttons.forEach(button => {
-  button.addEventListener("click", function() {
-    let value = this.value;
-    let currentDisplay = display.textContent;
-    let newDisplay;
+// buttons.forEach(button => {
+//   button.addEventListener("click", function() {
+//     let value = this.value;
+//     let currentDisplay = display.textContent;
+//     let newDisplay;
 
-    if (value === "clear") {
-      newDisplay = "0";
-    } else if (value === "backspace") {
-      newDisplay = currentDisplay.substring(0, currentDisplay.length - 1);
-    } else if (value === "=") {
-      newDisplay = eval(currentDisplay
+//     if (value === "clear") {
+//       newDisplay = "0";
+//     } else if (value === "backspace") {
+//       newDisplay = currentDisplay.substring(0, currentDisplay.length - 1);
+//     } else if (value === "=") {
+//       newDisplay = eval(currentDisplay) 
+  
+    
+
+
+
+        
